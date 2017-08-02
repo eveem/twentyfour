cf8a62c7b808029daea9218cf91f97d3264c2a9c
eveem <monpriya.tammavong@gmail.com>
2017-08-02 02:23:19 -0700

    Add draft method

--- twentyfour.py
+++ twentyfour.py
@@ -1,8 +1,16 @@
 import random
 
-command = "HI"
+def can_twentyfour ():
+	print ':',
+	return True
+
+command = "WELCOME TO TWENTY-FOUR GAMES"
 
 while (command != "exit"):
 	for i in xrange (4):
 		print random.randrange (1, 9),
+	if can_twentyfour ():
+		print '/',
+	else:
+		print 'X',
 	command = raw_input ()
