2865078287fb9536d66e1cfaa59e1ba84c9e8eb6
eveem <monpriya.tammavong@gmail.com>
2017-08-02 17:22:16 -0700

    Generate all operate

--- twentyfour.py
+++ twentyfour.py
@@ -1,12 +1,28 @@
 import random
 import itertools
 
+op = ['+', '-', '*', '/']
+
 def can_twentyfour ():
 	print ':',
-	alllist = list (itertools.permutations (arr))
-	for l in alllist:
-		print l
+	all_num = gen_permute_number ()
+	all_opt = gen_op ()
 	return True
+
+def gen_permute_number ():
+	return list (itertools.permutations (arr))
+
+def gen_op ():
+	all_op = []
+	for i in op:
+		for j in op:
+			for k in op:
+				temp = []
+				temp.append (i)
+				temp.append (j)
+				temp.append (k)
+				all_op.append (temp)
+	return all_op
 
 command = "WELCOME TO TWENTY-FOUR GAMES"
 
