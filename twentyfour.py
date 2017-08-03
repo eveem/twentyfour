cc7a3f6af2bddc97f472256668f670e8bfe79146
eveem <monpriya.tammavong@gmail.com>
2017-08-02 17:31:03 -0700

    Generate all equation complete

--- twentyfour.py
+++ twentyfour.py
@@ -1,28 +1,43 @@
 import random
 import itertools
 
-op = ['+', '-', '*', '/']
+operator = ['+', '-', '*', '/']
 
 def can_twentyfour ():
 	print ':',
 	all_num = gen_permute_number ()
 	all_opt = gen_op ()
+	for nm in all_num:
+		for ot in all_opt:
+			equation = merging (nm, ot)
+			# print equation
 	return True
 
 def gen_permute_number ():
 	return list (itertools.permutations (arr))
 
 def gen_op ():
-	all_op = []
-	for i in op:
-		for j in op:
-			for k in op:
+	op = []
+	for i in operator:
+		for j in operator:
+			for k in operator:
 				temp = []
 				temp.append (i)
 				temp.append (j)
 				temp.append (k)
-				all_op.append (temp)
-	return all_op
+				op.append (temp)
+	return op
+
+def merging (num, opt):
+	temp = []
+	p = 0
+	for i in xrange (7):
+		if i % 2 == 0:
+			temp.append (num[p])
+		else:
+			temp.append (opt[p])
+			p += 1
+	return temp
 
 command = "WELCOME TO TWENTY-FOUR GAMES"
 
