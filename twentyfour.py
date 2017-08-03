beea11389307754debecf7edb88a9777008ef715
eveem <monpriya.tammavong@gmail.com>
2017-08-02 17:08:37 -0700

    Generate all combination of four number

--- twentyfour.py
+++ twentyfour.py
@@ -1,16 +1,24 @@
 import random
+import itertools
 
 def can_twentyfour ():
 	print ':',
+	alllist = list (itertools.permutations (arr))
+	for l in alllist:
+		print l
 	return True
 
 command = "WELCOME TO TWENTY-FOUR GAMES"
 
 while (command != "exit"):
+	arr = []
 	for i in xrange (4):
-		print random.randrange (1, 9),
+		num = random.randrange (1, 9)
+		arr.append (num)
+	print arr,
 	if can_twentyfour ():
 		print '/',
 	else:
 		print 'X',
+
 	command = raw_input ()
